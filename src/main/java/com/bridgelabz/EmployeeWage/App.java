01da63e9578faa7167ac03537c122a3053213028
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-19 23:54:54 +0530

    Added: employee daily wage

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -11,11 +11,18 @@ public class App
         System.out.println( "Hello welcome to employee wage computation program on master branch." );
         
         int IS_PRESENT = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         int empCheck = (int) (Math.random() * 10) % 2;
         if(empCheck == IS_PRESENT) {
         	System.out.println("Employee is present");
+            empHrs = 8;
         }else {
         	System.out.println("Employee is absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee wage is : " + empWage);
     }
 }
