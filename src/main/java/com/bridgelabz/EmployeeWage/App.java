7a581be74e661a809826573713e60434d5a31359
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-20 01:22:08 +0530

    Added: condition on maximum working hours and days

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -13,10 +13,12 @@ public class App
         final int IS_FULL_TIME = 2;
         final int IS_PART_TIME = 1;
         final int MONTH_DAYS = 20;
+        final int WORKIN_HOURS = 100;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        for(int day = 1; day <= MONTH_DAYS; day++){
+        int days = 0;
+        while(empHrs < WORKIN_HOURS && days < MONTH_DAYS){
             int empCheck = (int) (Math.random() * 10) % 3;
             switch(empCheck) {
                 case IS_FULL_TIME:
@@ -28,7 +30,10 @@ public class App
                 default:
                     empHrs += 0;
             }
+            days++;
         }
+        System.out.println("Total woring days : " + days);
+        System.out.println("Total woring hours : " + empHrs);
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee wage for month is : " + empWage);
     }
