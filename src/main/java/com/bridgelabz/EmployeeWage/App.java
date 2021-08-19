790c654cfb0c0009ea487cff10d3f83b773687b7
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-19 23:35:03 +0530

    Added: employee is present or absent

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -10,5 +10,12 @@ public class App
     {
         System.out.println( "Hello welcome to employee wage computation program on master branch." );
         
+        int IS_PRESENT = 1;
+        int empCheck = (int) (Math.random() * 10) % 2;
+        if(empCheck == IS_PRESENT) {
+        	System.out.println("Employee is present");
+        }else {
+        	System.out.println("Employee is absent");
+        }
     }
 }
