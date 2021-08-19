1a6dcaa5eecd54641535a5a2551ca3c35d772de2
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-20 00:37:02 +0530

    Added: solving using switch case statement

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -10,21 +10,24 @@ public class App
     {
         System.out.println( "Hello welcome to employee wage computation program on master branch." );
         
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
+        final int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
         int empCheck = (int) (Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME) {
-        	System.out.println("Employee is present for full time");
-            empHrs = 8;
-        }else if(empCheck == IS_PART_TIME) {
-        	System.out.println("Employee is present for part time");
-            empHrs = 4;
-        }else {
-        	System.out.println("Employee is absent");
-            empHrs = 0;
+        switch(empCheck) {
+            case IS_FULL_TIME:
+            	System.out.println("Employee is present for full time");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME: 
+            	System.out.println("Employee is present for part time");
+                empHrs = 4;
+                break;
+            default:
+            	System.out.println("Employee is absent");
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee wage is : " + empWage);
