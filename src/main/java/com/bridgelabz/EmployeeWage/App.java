c9362bc83e961a137f17bdcff528acb32e46f600
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-19 23:13:57 +0530

    Welcome to the employee wage computation program on the master branch

---
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.EmployeeWage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello welcome to employee wage computation program on master branch." );
+        
+    }
+}
