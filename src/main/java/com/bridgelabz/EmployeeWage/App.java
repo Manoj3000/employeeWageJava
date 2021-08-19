c8623464be3896f3b80daf7753847158c843261d
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-20 00:26:06 +0530

    Added: full time and part time wage calculation

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -10,14 +10,18 @@ public class App
     {
         System.out.println( "Hello welcome to employee wage computation program on master branch." );
         
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) (Math.random() * 10) % 2;
-        if(empCheck == IS_PRESENT) {
-        	System.out.println("Employee is present");
+        int empCheck = (int) (Math.random() * 10) % 3;
+        if(empCheck == IS_FULL_TIME) {
+        	System.out.println("Employee is present for full time");
             empHrs = 8;
+        }else if(empCheck == IS_PART_TIME) {
+        	System.out.println("Employee is present for part time");
+            empHrs = 4;
         }else {
         	System.out.println("Employee is absent");
             empHrs = 0;
