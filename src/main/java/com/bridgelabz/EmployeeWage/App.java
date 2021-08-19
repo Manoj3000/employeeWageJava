02db545f0cf181eedc0b8b886b3c16a32f0953bc
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-20 00:56:20 +0530

    Added: calculating employee wage for a month

--- src/main/java/com/bridgelabz/EmployeeWage/App.java
+++ src/main/java/com/bridgelabz/EmployeeWage/App.java
@@ -12,24 +12,24 @@ public class App
         
         final int IS_FULL_TIME = 2;
         final int IS_PART_TIME = 1;
+        final int MONTH_DAYS = 20;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) (Math.random() * 10) % 3;
-        switch(empCheck) {
-            case IS_FULL_TIME:
-            	System.out.println("Employee is present for full time");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME: 
-            	System.out.println("Employee is present for part time");
-                empHrs = 4;
-                break;
-            default:
-            	System.out.println("Employee is absent");
-                empHrs = 0;
+        for(int day = 1; day <= MONTH_DAYS; day++){
+            int empCheck = (int) (Math.random() * 10) % 3;
+            switch(empCheck) {
+                case IS_FULL_TIME:
+                    empHrs += 8;
+                    break;
+                case IS_PART_TIME: 
+                    empHrs += 4;
+                    break;
+                default:
+                    empHrs += 0;
+            }
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee wage is : " + empWage);
+        System.out.println("Employee wage for month is : " + empWage);
     }
 }
